5444059b6324ba9ccea5a37926df95fa3d7fa055
Raito Bezarius <masterancpp@gmail.com>
2024-09-07 17:18:59 +0200

    feat(hostapd): enable ubus on RADIUS variant
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- overlay.nix
+++ overlay.nix
@@ -172,9 +172,18 @@ extraPkgs // {
       "CONFIG_LIBNL32=y"
       "CONFIG_PKCS12=y"
       "CONFIG_RSN_PREAUTH=y"
+      "CONFIG_UBUS=y"
       "CONFIG_TLS=internal"
     ];
     h = prev.hostapd.overrideAttrs(o: {
+      buildInputs = o.buildInputs ++ [ final.libubox final.ubus ];
+        src = final.fetchFromGitea {
+           domain = "git.dgnum.eu";
+           owner = "DGNum";
+           repo = "hostapd";
+           rev = "hostap-liminix-integration";
+           hash = "sha256-qoCXx3raXCD51YX5izj30VG/HMgr6lv/288Yg9I4S7M=";
+      };
       extraConfig = "";
       configurePhase = ''
         cat > hostapd/defconfig <<EOF
