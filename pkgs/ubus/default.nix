036f91d2f0990404dba2bb643da7642ad7f5673e
Raito Bezarius <masterancpp@gmail.com>
2024-09-08 01:08:59 +0200

    feat(ubus): support for Lua 5.3
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- pkgs/ubus/default.nix
+++ pkgs/ubus/default.nix
@@ -1,13 +1,14 @@
-{ stdenv, fetchFromGitHub, cmake, libubox, json_c, lua5_1, defaultSocketLocation ? "/run/ubus/ubus.sock" }:
+{ stdenv, fetchFromGitea, lib, cmake, libubox, json_c, lua, defaultSocketLocation ? "/run/ubus/ubus.sock" }:
 stdenv.mkDerivation {
   pname = "ubus";
   version = "unstable-04-09-2024";
 
-  src = fetchFromGitHub {
-    owner = "openwrt";
+  src = fetchFromGitea {
+    domain = "git.dgnum.eu";
+    owner = "DGNum";
     repo = "ubus";
-    rev = "65bb027054def3b94a977229fd6ad62ddd32345b";
-    hash = "sha256-n82Ub0IiuvWbnlDCoN+0hjo/1PbplEbc56kuOYMrHxQ=";
+    rev = "ebb1dc92e4985538a8e18b7e926264118138f281";
+    hash = "sha256-fo4zleC9R6uzlcOJ/jQ0t0nSBHUAq/uqPVd9xJdkAM0=";
   };
 
   # We don't use /var/run/ in Liminix by default.
@@ -21,14 +22,14 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
-    lua5_1
+    lua
     libubox
     json_c
   ];
 
   cmakeFlags = [
     "-DBUILD_LUA=on"
-    "-DLUAPATH=${placeholder "out"}/lib/lua"
+    "-DLUAPATH=${placeholder "out"}/lib/lua/${lua.luaversion}"
     "-DBUILD_EXAMPLES=off"
   ];
 }
